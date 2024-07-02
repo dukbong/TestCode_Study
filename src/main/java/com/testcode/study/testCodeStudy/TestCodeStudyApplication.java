21cd0fada16df46e91f36e565c91aed9ff926bce
janghs <gorae930301@gmail.com>
2024-07-02 12:34:36 +0900

    frist commit 🎉

---
+++ src/main/java/com/testcode/study/testCodeStudy/TestCodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.testcode.study.testCodeStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodeStudyApplication.class, args);
+	}
+
+}
